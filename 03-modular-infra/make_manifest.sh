119386e80a3d0ebd9b98642b8380119f849bbec8
Dr Nic Williams <drnicwilliams@gmail.com>
2015-03-31 15:28:13 -0700

    03-modular-infra

---
+++ 03-modular-infra/make_manifest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+dir="03-modular-infra"
+
+infra=$1; shift
+if [[ "${infra}" != "aws" && "${infra}" != "bosh-lite" ]]; then
+  echo "USAGE: make_manifest.sh aws|bosh-lite"
+  exit 1
+fi
+
+cd $dir
+spiff merge cf-deployment.yml cf-infra-$infra.yml sandbox-stub.yml
+
+cd ..
